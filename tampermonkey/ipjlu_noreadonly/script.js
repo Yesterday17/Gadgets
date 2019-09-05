acfe1fbe0250eb1386fa8c2c373c006f6a3f8680
Yesterday17 <t@yesterday17.cn>
2019-09-05 09:13:03 +0800

    ipjlu_noreadonly

---
+++ tampermonkey/ipjlu_noreadonly/script.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         吉大校园网自助营业厅密码输入
+// @namespace    https://github.com/Yesterday17/Gadgets
+// @version      1.0.0
+// @description  解除吉林大学校园网自助营业厅密码输入环节的强制手动输入。
+// @author       Yesterday17
+// @match        https://ip.jlu.edu.cn/pay/*
+// @run-at       document-start
+// @supportURL   https://github.com/Yesterday17/Gadgets/issues
+// ==/UserScript==
+
+(() => {
+  document.querySelectorAll('[readonly]').forEach(ro => {
+    ro.removeAttribute('readonly');
+    ro.removeAttribute('onclick');
+  });
+})();
